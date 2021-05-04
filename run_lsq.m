9b1f3515b8935d127e8be5d893b486bd11ec5d65
ani-per <amp7226@psu.edu>
2021-05-04 16:32:52 -0400

    Least-squares setup ready for mass-inertia

--- run_lsq.m
+++ run_lsq.m
@@ -20,10 +20,14 @@ t_test = squeeze(permute(train_data.t, [2, 3, 1]));
 U_test = squeeze(permute(test_data.U, [2, 3, 1]));
 Z_test = squeeze(permute(test_data.Z, [2, 3, 1]));
 n_sim = size(t_train, 2);
+dt = t_train(2, 1) - t_train(1, 1);
 
 i_start_train = zeros(n_sim, 1) - 1;
 t_start_train = zeros(n_sim, 1) - 1;
 v_x_train = zeros(n_sim, 1) - 1;
+i_start_test = zeros(n_sim, 1) - 1;
+t_start_test = zeros(n_sim, 1) - 1;
+v_x_test = zeros(n_sim, 1) - 1;
 for n = 1:n_sim
     U_n = Z_train(3, :, n);
     for i = 1:length(U_n)
@@ -35,12 +39,6 @@ for n = 1:n_sim
             break 
         end
     end
-end
-
-i_start_test = zeros(n_sim, 1) - 1;
-t_start_test = zeros(n_sim, 1) - 1;
-v_x_test = zeros(n_sim, 1) - 1;
-for n = 1:n_sim
     U_n = Z_test(3, :, n);
     for i = 1:length(U_n)
         pf = polyfit(t_test(i:end, n), U_n(:, i:end), 1);
@@ -53,9 +51,56 @@ for n = 1:n_sim
     end
 end
 
-% input = [2];
-% U_train = U_train(input, :);
-% U_test = U_train(input, :);
-% obs = [2, 4, 5, 6];
-% Z_train = Z_train(obs, :, :);
-% Z_test = Z_test(obs, :, :);
+input = [2];
+U_train = U_train(input, :, :);
+U_test = U_test(input, :, :);
+obs = [4, 6];
+Z_train = Z_train(obs, :, :);
+Z_test = Z_test(obs, :, :);
+
+l = 5;
+c = -10000;
+m_train = zeros(n_sim, 1);
+I_train = zeros(n_sim, 1);
+for sim = 1:n_sim
+    a = lsq_mi(U_train(:, :, sim)', Z_train(:, :, sim)', dt, v_x_train(sim), l, c);
+    m_train(sim) = 1/a(1);
+    I_train(sim) = 1/a(2);
+end
+
+n_bins = 20;
+mi_hist = figure("Position", fig_size);
+tiles = tiledlayout(2, 1, "Padding", "none", "TileSpacing", "compact");
+nexttile;
+histogram(m_train, n_bins);
+xlabel("Mass ($m$) [$kg$]");
+nexttile;
+histogram(I_train, n_bins);
+xlabel("Mass ($m$) [$kg$]");
+title(tiles, ...
+    [sprintf("Histogram of Parameters Estimated via Least-Squares over %i Simulations", n_sim); ...
+    sprintf("$l = %i$ m, $c = %i$ $\\frac{N}{rad}$", l, c)], ...
+    "Interpreter", "Latex", "FontSize", font_size);
+export_fig(fullfile(root, "hist_m-I"), "-pdf", "-png");
+
+function [a] = lsq_mi(u, x, dt, v_x, l, c)
+    arguments
+        u (:, 1) double
+        x (:, 2) double
+        dt (1, 1) double
+        v_x (1, 1) double
+        l (1, 1) double = 5
+        c (1, 1) double = -10000
+    end
+
+    assert(height(u) == height(x));
+    
+    nt = height(u);
+    v_y = x(1:(end - 1), 1);
+    r = x(1:(end - 1), 2);
+    delta = u(1:(end - 1));
+    
+    x_fd = reshape(diff(x), [], 1)/dt + [v_x*r; zeros(size(r))];
+    H = [(2*c/v_x)*v_y - c*delta, zeros(nt - 1, 1); zeros(nt - 1, 1), ((l^2*c)/(2*v_x))*r - (l*c/2)*delta];
+    a = (H'*H)\H'*x_fd;
+end
